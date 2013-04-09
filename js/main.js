d9796d2cf5866d663cc5ee323d7e5e5f6dc8caf1
Paul <halpo@me.com>
2013-04-09 15:58:01 +0100

    sd

--- js/main.js
+++ js/main.js
@@ -1,13 +1,1 @@
-$(document).ready(function() {
-	var windowSize = $(window).width();
-	var originalFontSize = windowSize/35.0;
-	var sectionWidth = $('#sidebar').width();
 
-	if (windowSize>=800) {
-		$('#sidebar span').each(function(){
-			var spanWidth = $(this).width();
-			var newFontSize = (sectionWidth/spanWidth) * originalFontSize;
-			$(this).css({"font-size" : newFontSize, "line-height" : newFontSize/1.2 + "px"});
-		});
-	}
-});
