ac335368561012076f1d39ef3f7886cf4876ef52
Paul <halpo@me.com>
2013-04-09 15:17:59 +0100

    f

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	var windowSize = $(window).width();
+	var originalFontSize = windowSize/35.0;
+	var sectionWidth = $('#sidebar').width();
+
+	$('#sidebar span').each(function(){
+		var spanWidth = $(this).width();
+		var newFontSize = (sectionWidth/spanWidth) * originalFontSize;
+		$(this).css({"font-size" : newFontSize, "line-height" : newFontSize/1.2 + "px"});
+	});
+});
