e3980a5f6439a00fad87bc3c880528910a283325
Michael Battcock <mbattcock01@gmail.com>
2013-04-24 14:36:03 +0100

    scroll on fade bug fix

--- js/main.js
+++ js/main.js
@@ -1,22 +1,31 @@
 $(document).ready(function() {
 	$('.fade-on-scroll > :not(.seperator)').each(function () { 
-        a = $(this).offset().top + ($(this).height() / 3);
+        a = $(this).offset().top;
         console.log(a)
-        b = $(window).scrollTop() + $(window).height() + 150;
+        b = $(window).scrollTop() + $(window).height() + 110; // plus a bit extra so there's no gaps
         console.log(b)
         if (a < b) {
         	$(this).css('opacity',1);
         }
 	});
+
+	$('#word-cloud').attr('style', function(i, style) {
+	    return style.replace(/display[^;]+;?/g, '');
+	});
 });
 
 $(window).scroll(function(d,h) {
 	$('.fade-on-scroll > :not(.seperator)').each(function () { 
-        a = $(this).offset().top + ($(this).height() / 2);
+        a = $(this).offset().top + 100;	// fade in once we get 100 pixels past the top of the element
         b = $(window).scrollTop() + $(window).height();
         if (a < b) {
         	$(this).fadeTo(500,1);
         }
 	});
+
+	$('#word-cloud').attr('style', function(i, style) {
+	    return style.replace(/display[^;]+;?/g, '');
+	});
 });
+
 
