d1769f89063394b7aa6a21118a778980366e005b
Michael Battcock <mbattcock01@gmail.com>
2013-04-24 13:41:51 +0100

    fade on scroll window ready fix

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+$(window).ready(function() {
+	$('.fade-on-scroll > *').each(function () { 
+        a = $(this).offset().top + ($(this).height() / 2);
+        b = $(window).scrollTop() + $(window).height();
+        if (a < b) {
+        	$(this).css('opacity',1);
+        }
+	});
+});
+
 $(window).scroll(function(d,h) {
 	$('.fade-on-scroll > *').each(function () { 
         a = $(this).offset().top + ($(this).height() / 2);
@@ -7,3 +17,4 @@ $(window).scroll(function(d,h) {
         }
 	});
 });
+
