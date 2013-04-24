1a205c6cc018840830614eb8c50448bdcddee6bb
Michael Battcock <mbattcock01@gmail.com>
2013-04-24 13:35:12 +0100

    fade on scroll

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
-// $('#word-cloud').masonry({
-//   itemSelector: '.box',
-//   columnWidth: 100,
-//   isAnimated: true
-// });
+$(window).scroll(function(d,h) {
+	$('.fade-on-scroll > *').each(function () { 
+        a = $(this).offset().top + ($(this).height() / 2);
+        b = $(window).scrollTop() + $(window).height();
+        if (a < b) {
+        	$(this).fadeTo(500,1);
+        }
+	});
+});
